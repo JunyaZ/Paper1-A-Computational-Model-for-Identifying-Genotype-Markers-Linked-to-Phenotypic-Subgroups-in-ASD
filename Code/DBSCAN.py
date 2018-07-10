6bf0f5282fa91dc5cb3e07ebdbe8458145f886b8
Junya Zhao <31264540+JunyaZ@users.noreply.github.com>
2018-07-10 10:33:30 -0500

    Add files via upload

--- Code/DBSCAN.py
+++ Code/DBSCAN.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  9 12:58:03 2018
+
+@author: ya000
+"""
+
 import numpy as np
 import pandas as pd
 from collections import Counter
@@ -6,27 +13,25 @@ from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
-
-%matplotlib inline
-
-data = pd.read_csv('159_matrix_numminmax_Nom.csv',header = None) 
 # #############################################################################
-
+#data = pd.read_csv('winequality-red.csv') 
+data = pd.read_csv('156_matrix_binary_output.csv',header = None) 
+#data = pd.read_csv('153_matrix_all_num.csv')
+#data = pd.read_csv('test.csv',header= None) 
 def CateFunc(x,y):
     return (np.sum(np.not_equal(x,y)))/20
-
+# #############################################################################
 # Compute DBSCAN
+
 model = DBSCAN(eps=1.8, min_samples=3 ,metric = CateFunc).fit(data)
 outliers_df = pd.DataFrame(data)
 print(model)
 print (Counter(model.labels_))
 print(model.labels_)
+print("=====")
 #np.savetxt("labels_dascan.csv", model.labels_)
-outliers_df[model.labels_== -1]
-
-
-kmeans = KMeans(n_clusters=500, random_state=0).fit(data)
-kmeans.labels_
-print (Counter(kmeans.labels_))
-#np.savetxt("labels_kmeans.csv", model.labels_)
-
+print(outliers_df[model.labels_== -1])
+for i in range(-1,299):
+    outliers_df[model.labels_== i].to_csv(f"{'label'+str(i)}.csv")
+        
+        
